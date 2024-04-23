30b14a7cdf989683fc7e73d6ac6814ddd0c25a3e
pgwart <76925040+pgwart@users.noreply.github.com>
2024-04-23 16:03:53 -0500

    Changed to make fitting easier

--- RPproject.py
+++ RPproject.py
@@ -70,12 +70,17 @@ def gal_blackbody(f, Mmax=120):
     BB = [Balmer(T_from_M(m),f)*IMF(m)*dM(T_from_M(m))*sp.blackbody_intensity(T_from_M(m),f) for m in M]
     return np.trapz(BB, x=M)
 
+def gal_blackbody_no_balmer(f, Mmax=120):
+    M = np.geomspace(0.05,Mmax)
+    BB = [IMF(m)*dM(T_from_M(m))*sp.blackbody_intensity(T_from_M(m),f) for m in M]
+    return np.trapz(BB, x=M)   
+
 def exp_SFR(t, tau=None):
     if not tau:
         tau = 1e8
     return np.exp(-t/tau)
 
-def star_formation_blackbody(f, t1, t2, SFR=None, tau=None):
+def star_formation_blackbody(f, t1, t2, SFR=None, tau=None, balmer=True):
     tstart = np.max([t1,t2])
     tstop = np.min([t1,t2])
     time = np.linspace(tstart, tstop, 20)
@@ -83,16 +88,21 @@ def star_formation_blackbody(f, t1, t2, SFR=None, tau=None):
     
     # SFR is the star formation rate function
     # If not given, assume constant star formation rate
+    if balmer:
+        bbfunc = gal_blackbody
+    else:
+        bbfunc = gal_blackbody_no_balmer
+        
     if SFR:
-        BB = [-1*SFR(t,tau)*gal_blackbody(f, M)*dt(M) for (t,M) in zip(time, Mmax)]
-    else:   
-        BB = [-1*gal_blackbody(f, M)*dt(M) for (t,M) in zip(time, Mmax)]
+        BB = [-1*SFR(t,tau)*bbfunc(f, M)*dt(M) for (t,M) in zip(time, Mmax)]
+    else:
+        BB = [-1*bbfunc(f, M)*dt(M) for (t,M) in zip(time, Mmax)]
     return np.trapz(BB,x=Mmax)
 
-def generate_blackbody(farray, t1, t2, SFR=None, tau=None):
+def generate_blackbody(farray, t1, t2, SFR=None, tau=None, balmer=True):
     res = []
     for f in farray:
-        B = star_formation_blackbody(f, t1, t2, SFR=None, tau=None)
+        B = star_formation_blackbody(f, t1, t2, SFR=SFR, tau=tau, balmer=balmer)
         if B == np.nan:
             B = 0
         res.append(B)
